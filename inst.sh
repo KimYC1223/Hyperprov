cba7cab7caabde1db30cd7ca057cc31b2a82877a
KimYC1223 <kau_esc@naver.com>
2019-07-29 06:27:09 +0900

    efdsafds

--- inst.sh
+++ inst.sh
@@ -1,1 +1,1 @@
-docker swarm join --token SWMTKN-1-2tuoj0i0dq7cszcyxvvhqj2net89nysz85o2db6k1vt7f109rn-8613fgah410pyvs95a50oszws 192.168.0.5:2377
+docker swarm join --token SWMTKN-1-3za9ijcgbvhunl56ynuhcl6u72u19cel6c8x2u3wxbbcv48vmi-dh1snl5vtdfpwkzb7notferoo 192.168.0.5:2377
