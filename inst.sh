8a13df47ba5c4841dc558f86cfe31f0229bb6a64
KimYC1223 <kau_esc@naver.com>
2019-07-29 06:05:09 +0900

    test123

--- inst.sh
+++ inst.sh
@@ -1,1 +1,1 @@
-docker swarm join --token SWMTKN-1-0puo6ct333xs6ne6r78f2lu2li6uyla7vgphvyzjo3ji3wpbu0-2gw3vjc9kyp585uej6o1yxffh 192.168.0.5:2377
+docker swarm join --token SWMTKN-1-0omkw0t8wi3416f9uri6bysefwz46ybq8a9o0zjvqrm50dcq62-3a6rh9shq7fmh4rn83353ox1p 192.168.0.5:2377
