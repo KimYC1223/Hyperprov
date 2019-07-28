e0f5a663946e3077ff5599c633f6212137b5529a
KimYC1223 <kau_esc@naver.com>
2019-07-29 06:18:41 +0900

    fdsalkfs

--- inst.sh
+++ inst.sh
@@ -1,1 +1,1 @@
-docker swarm join --token SWMTKN-1-0omkw0t8wi3416f9uri6bysefwz46ybq8a9o0zjvqrm50dcq62-3a6rh9shq7fmh4rn83353ox1p 192.168.0.5:2377
+docker swarm join --token SWMTKN-1-2tuoj0i0dq7cszcyxvvhqj2net89nysz85o2db6k1vt7f109rn-8613fgah410pyvs95a50oszws 192.168.0.5:2377
