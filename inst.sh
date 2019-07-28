709b03f9004e159893fe4c402c5f13e949798cdd
KimYC1223 <kau_esc@naver.com>
2019-07-29 06:35:03 +0900

    dfadslfas

--- inst.sh
+++ inst.sh
@@ -1,1 +1,1 @@
-docker swarm join --token SWMTKN-1-3za9ijcgbvhunl56ynuhcl6u72u19cel6c8x2u3wxbbcv48vmi-dh1snl5vtdfpwkzb7notferoo 192.168.0.5:2377
+docker swarm join --token SWMTKN-1-2jfg3lg5r4qlsjg9b1pq37iah6wm6j4u1r6ufuqcpbjdjx2zjm-de3g4twddjbpelbb1x7ub0gxz 192.168.0.5:2377
