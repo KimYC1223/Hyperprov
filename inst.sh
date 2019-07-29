203573b85cf7bbacd6d6cc5fa2522160c8d20e67
Your Name <you@example.com>
2019-07-30 04:41:41 +0900

    asdasdasdasd

--- inst.sh
+++ inst.sh
@@ -1,1 +1,1 @@
-docker swarm join --token SWMTKN-1-5d8n99ldp0f7ph3065y6tkleq14x6fqae2vbvryiok3bpr6as5-9q2sikelrgxl0ol0loimlisvv 192.168.9.107:2377
+docker swarm join --token SWMTKN-1-0oakr51difw6fqxkqztsvrol4uzrl5iwf4psfsazchbicmrmbp-09ls8b1nzjdohsvpd3lixmmu4 192.168.9.107:2377
