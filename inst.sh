66c2a6135681a7e6186fd6d678a889e9e5cfaccb
KimYC1223 <kau_esc@naver.com>
2019-07-31 21:51:30 +0900

    asdasd2

--- inst.sh
+++ inst.sh
@@ -1,1 +1,1 @@
-docker swarm join --token SWMTKN-1-0uhu6pmfg2bd4pt6g6arcywg2lc4u4on1ug7ba4e57jgogp7f5-dhsv0w9d424k3iricml0lenao 192.168.0.5:2377
+docker swarm join --token SWMTKN-1-2623lpa7xswtwayk6qlnhtoa0sjg3fyia1ugsl5rzrjotv8e6h-2iy7o0e4j5bmsedb81raspf2a 192.168.0.5:2377
