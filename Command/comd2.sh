03f094d188405a5354749664ceb212213c77b3aa
KimYC1223 <kau_esc@naver.com>
2019-07-31 18:10:12 +0900

    remove

--- Command/comd2.sh
+++
@@ -1,1 +0,0 @@
-docker swarm join --token SWMTKN-1-3gy95hpko63ybjvdhdi3yvv1n6z2eowpk07isn17lb1kbh04xi-6mpu3x1axkq74eauimdi8codr 192.168.9.107:2377
--- inst.sh
+++
@@ -1,1 +0,0 @@
-docker swarm join --token SWMTKN-1-0oakr51difw6fqxkqztsvrol4uzrl5iwf4psfsazchbicmrmbp-09ls8b1nzjdohsvpd3lixmmu4 192.168.9.107:2377
